Fixed erroneously ordered covariance matrix in union2.1, emptied cache and settled on final inference parameters

analyze_stored_data.py
@@ -5,16 +5,22 @@ from utilitites import *
 import nifty8 as ift
 from setup_cosmological import *
 
-samples = unpickle_me_this("real/pantheon+_cfm_{'offset_mean': 0, 'offset_std': None, 'fluctuations': (1.8, 1.8), 'loglogavgslope': (-4, 1e-16), 'asperity': None, 'flexibility': None}_lm_{'slope': (2, 5), 'intercept': (30, 30)}.pickle")
+samples = unpickle_me_this("real/pantheon+_cfm_{'offset_mean': 0, 'offset_std': None, 'fluctuations': (0.06, 0.03), 'loglogavgslope': (-4, 1e-16), 'asperity': None, 'flexibility': None}_lm_{'slope': (2, 5), 'intercept': (30, 5)}.pickle",
+                           absolute_path=False)
 
 # plot_flat_lcdm_fields(x_max=np.max(np.log(1 + z_p)), show=True, save=False)  # Creates a plot of the comparison CMB and SN fields
 # plot_charm1_in_comparison_fields(x_max_pn=np.max(np.log(1 + z_p)), x_max_union=np.max(np.log(1+z_u)), show=False,
 #                                save=True)
 
-
 # Extract and visualize posterior samples
 posterior_realizations_list = samples
+
+# Signal Space Comparison Visualization
 s_mean, s_var = posterior_realizations_list.sample_stat(s)
+plot_charm2_in_comparison_fields(x_max_pn=np.max(np.log(1 + z_p)), x_max_union=np.max(np.log(1+z_u)), show=True,
+                                 save=False, x=x.field().val, s=X.adjoint(s_mean).val,
+                                 s_err=np.sqrt(X.adjoint(s_var).val))
+
 
 plot_data_comparison = False
 if plot_data_comparison:
@@ -26,23 +32,25 @@ else:
     s_mean = s_mean
     s_var = s_var
 
-# Signal Space Comparison Visualization
-plot_charm2_in_comparison_fields(x_max_pn=np.max(np.log(1 + z_p)), x_max_union=np.max(np.log(1+z_u)), show=False,
-                                 save=True, x=x.field().val, s=X.adjoint(s_mean).val,
-                                 s_err=np.sqrt(X.adjoint(s_var).val))
 
 if plot_data_comparison:
 
     # Reconstruct data from posterior
-    reconstructed_data = build_response(x, ift.Field.from_raw(x, s_mean.val), data_space, neg_a_mag)
-    data_from_base_lcdm = build_response(x, ift.Field.from_raw(x, base_s), data_space, neg_a_mag)
+    print("comparison for PANTHEON Compilation dataset (if not change code here)")
+    reconstructed_data = build_response(x, ift.Field.from_raw(x, s_mean.val), data_space_p, neg_a_mag_p)
+    s_cmb, s_cmb_err = build_flat_lcdm(x.field().val, mode='CMB')
+    s_sn, s_sn_err = build_flat_lcdm(x.field().val, mode='SN')
+    data_from_cmb_lcdm = build_response(x, ift.Field.from_raw(x, s_cmb), data_space_p, neg_a_mag_p)
+    data_from_sn_lcdm = build_response(x, ift.Field.from_raw(x, s_sn), data_space_p, neg_a_mag_p)
 
-    print("Chi Squared between data and base lcdm: ", chi_square(mu, data_from_base_lcdm.val))
-    print("Chi Squared between data and reconstruction: ", chi_square(mu, reconstructed_data.val))
+    print("Chi Squared between data and base lcdm CMB: ", chi_square(mu_p, data_from_cmb_lcdm.val))
+    print("Chi Squared between data and base lcdm SN: ", chi_square(mu_p, data_from_sn_lcdm.val))
+    print("Chi Squared between data and RECONSTRUCTION: ", chi_square(mu_p, reconstructed_data.val))
 
     # Data Space Comparison Visualization
-    plt.plot(neg_a_mag, mu, ".", color="black", label=r"Real $\mu$")
-    plt.plot(neg_a_mag, data_from_base_lcdm.val, ".", label="Data from base", color="red")
-    create_plot_1(x=neg_a_mag, y=reconstructed_data.val, color=blue, x_label="$x$", y_label=r"Distance modulus $\mu$",
+    plt.plot(neg_a_mag_p, mu_p, ".", color="black", label=r"Real $\mu$")
+    plt.plot(neg_a_mag_p, data_from_cmb_lcdm.val, ".", label="Data from base CMB", color="red")
+    plt.plot(neg_a_mag_p, data_from_sn_lcdm.val, ".", label="Data from base SN", color="green")
+    create_plot_1(x=neg_a_mag_p, y=reconstructed_data.val, color=blue, x_label="$x$", y_label=r"Distance modulus $\mu$",
                   title="Data reconstruction from base and reconstruction", legend_label="Data from Reconstruction")
 

cosmological_tomography.py
@@ -2,7 +2,7 @@ from setup_cosmological import *
 
 # likelihood_energy_union = ift.GaussianEnergy(d_u, N_u.inverse) @ R_u  # Union2.1 likelihood
 likelihood_energy_pantheon = ift.GaussianEnergy(d_p, N_p.inverse) @ R_p  # Pantheon+ likelihood
-global_iterations = 6
+global_iterations = 3
 
 posterior_samples = ift.optimize_kl(likelihood_energy=likelihood_energy_pantheon,
                                     total_iterations=global_iterations,
@@ -12,10 +12,17 @@ posterior_samples = ift.optimize_kl(likelihood_energy=likelihood_energy_pantheon
                                     nonlinear_sampling_minimizer=geoVI_sampling_minimizer,
                                     return_final_position=False,
                                     resume=True,
-                                    output_directory="output_helper")
+                                    output_directory=f"output_helper")
 # Save the inference run
 pickle_me_this(f"real/pantheon+_{arguments}", posterior_samples)
 print("Saved posterior samples as pickled object. Parameters of the inference: \n",
       arguments,
       "\nUse `analyze_stored_data` to visualize results.")
+
+s_mean, s_var = posterior_samples.sample_stat(s)
+
+# Signal Space Comparison Visualization
+plot_charm2_in_comparison_fields(x_max_pn=np.max(np.log(1 + z_p)), x_max_union=np.max(np.log(1+z_u)), show=True,
+                                 save=True, x=x.field().val, s=X.adjoint(s_mean).val,
+                                 s_err=np.sqrt(X.adjoint(s_var).val))
 

setup_cosmological.py
@@ -14,13 +14,16 @@ import nifty8 as ift
 # Read raw data. quantity_u := Union2.1 data; quantity_p := Pantheon+ data.
 z_u, mu_u, covariance_u = read_data_union()
 z_p, mu_p, covariance_p = read_data_pantheon()
+z_d, mu_d, covariance_d = read_data_des()
+
+
+print("Points lying higher than z=1.12 (x=0.75) in pantheon: ", len(z_p[np.where(z_p > 1.12)]))
 
 config = {
     'Signal Field Resolution': 1024,  # 2**10
     'Length of signal space': np.max(np.log(1+z_p)),  # x_max. Has to be set manually according to which data is used
     'Factor to extend signal space size by': 1.3,
 }
-
 n_pix, x_length, x_fac = [float(setting) for setting in config.values()]
 # n_dp_u = len(z_u)
 n_dp_p = len(z_p)
@@ -32,10 +35,8 @@ x_ext = ift.RGSpace(x_fac * n_pix, distances=pxl_size)  # The signal space exten
 
 x = attach_custom_field_method(x)  # Attach the custom `field` method.
 x_ext = attach_custom_field_method(x_ext)  # Attach the custom `field` method.
-
 # data_space_u = ift.UnstructuredDomain((n_dp_u,))
 data_space_p = ift.UnstructuredDomain((n_dp_p,))
-
 
 # neg_a_mag_u = np.log(1+z_u)  # The negative scale factor magnitude, x = -log(a) = log(1+z)
 neg_a_mag_p = np.log(1+z_p)
@@ -44,7 +45,7 @@ neg_a_mag_p = np.log(1+z_p)
 args_cfm = {
     'offset_mean': 0,
     'offset_std': None,
-    'fluctuations': (1.8, 1.8),
+    'fluctuations': (.4, .2),
     'loglogavgslope': (-4, 1e-16),
     'asperity': None,
     'flexibility': None,
@@ -53,7 +54,7 @@ args_cfm = {
 # Arguments of the line model
 args_lm = {
     'slope': (2, 5),
-    'intercept': (30, 30)
+    'intercept': (30, 10)
 }
 
 X = ift.FieldZeroPadder(domain=x, new_shape=(x_fac*n_pix, ))
@@ -62,9 +63,12 @@ X = ift.FieldZeroPadder(domain=x, new_shape=(x_fac*n_pix, ))
 s = ift.SimpleCorrelatedField(target=x_ext, **args_cfm) + LineModel(target=x_ext, args=args_lm)
 arguments = 'cfm_' + str(args_cfm) + '_lm_' + str(args_lm)
 
+# s = LCDM_MODEL(x_ext)
+# plot_prior_distribution(mean_std_tuple=(.1, 1e-16), distribution_name="lognormal", n_samples=100)
 # Build the signal response, noise operator, data field and others
 # R_u = build_response(signal_space=x, signal=X.adjoint @ s, data_space=data_space_u, neg_scale_factor_mag=neg_a_mag_u)
 R_p = build_response(signal_space=x, signal=X.adjoint @ s, data_space=data_space_p, neg_scale_factor_mag=neg_a_mag_p)
+# ift.plot_priorsamples(s)
 # N_u = CovarianceMatrix(domain=data_space_u, matrix=covariance_u, sampling_dtype=np.float64, tol=1e-4)
 N_p = CovarianceMatrix(domain=data_space_p, matrix=covariance_p, sampling_dtype=np.float64, tol=1e-4)
 # d_u = ift.Field(domain=ift.DomainTuple.make(data_space_u,), val=mu_u)

setup_synthetic.py
@@ -12,13 +12,14 @@ import numpy as np
 # Define iteration and minimization control
 
 n_dp = 500
-neg_a_mag = unidirectional_radial_los(n_dp)  # The negative scale factor magnitude, x = -log(a) = log(1+z)
+neg_a_mag = unidirectional_radial_los(n_dp, uniform_drawing=True)  # The negative scale factor magnitude,
+# x = -log(a) = log(1+z)
 
 config = {
-    'Signal Field Resolution': 1024,
+    'Signal Field Resolution': 4096,
     'Length of signal space': np.max(neg_a_mag),
     'Fac to extend signal space by': 1.3,
-    'Noise level': .05,
+    'Noise level': .1,
 }
 
 n_pix, x_length, x_fac, noise_level = [float(setting) for setting in config.values()]
@@ -34,7 +35,7 @@ data_space = ift.UnstructuredDomain((n_dp,))
 args_cfm = {
     'offset_mean': 0,
     'offset_std': None,
-    'fluctuations': (1.8, 0.1),
+    'fluctuations': (.4, .2),
     'loglogavgslope': (-4, 1e-16),
     'asperity': None,
     'flexibility': None,
@@ -43,22 +44,23 @@ args_cfm = {
 # Arguments of the line model
 args_lm = {
     'slope': (2, 5),
-    'intercept': (30, 5)
+    'intercept': (30, 10)
 }
 
 X = ift.FieldZeroPadder(domain=x, new_shape=(x_fac*n_pix, ))
 
 # The to-be-inferred signal on the extended domain
 s = ift.SimpleCorrelatedField(target=x_ext, **args_cfm) + LineModel(target=x_ext, args=args_lm)
-
+ift.plot_priorsamples(s)
 # The ground truth model
-s_g = synthetic_ground_model_truth(signal_space=x_ext)
+s_g = PiecewiseLinear(signal_space=x_ext, omega_m_custom=0.3, omega_l_custom=2)
 
 arguments = 'cfm_' + str(args_cfm) + '_lm_' + str(args_lm)
 
 # Build the signal response, noise operator, data field and others
 R = build_response(signal_space=x, signal=X.adjoint @ s, data_space=data_space, neg_scale_factor_mag=neg_a_mag)
 R_g = build_response(signal_space=x, signal=X.adjoint @ s_g, data_space=data_space, neg_scale_factor_mag=neg_a_mag)
+# Ground truth response
 N = ift.ScalingOperator(domain=(data_space, ), factor=noise_level, sampling_dtype=np.float64)
 
 # Iteration control for `MGVI` and linear parts of the inference
@@ -74,7 +76,3 @@ geoVI_sampling_minimizer = ift.NewtonCG(ic_sampling_nl)
 ic_newton = ift.AbsDeltaEnergyController(name='Newton Descent Finder', deltaE=0.1, convergence_level=2,
                                          iteration_limit=35)
 descent_finder = ift.NewtonCG(ic_newton)
-
-raise_warning("\nUnion2.1 covariance matrix is only symmetric up to a factor of 10^{-10}.\n"
-              "Pantheon+ covariance matrix is only symmetric up to a factor of 10^{-4}.\n\n")
-

synthetic_inference.py
@@ -1,6 +1,6 @@
 from setup_synthetic import *
-from setup_cosmological import z_p
-from setup_cosmological import mu_u, mu_p
+from setup_cosmological import z_p  # get real data for plotting purposes
+from setup_cosmological import mu_u, mu_p  # get real data for plotting purposes
 
 # Construct random ground truth domain field
 ground_truth_model = ift.from_random(s_g.domain)
@@ -10,13 +10,14 @@ ground_truth_field = s_g(ground_truth_model)
 d = R_g(ground_truth_model) + N.draw_sample()
 
 # Plot signal field ground truth, as well as data realizations
-plot_synthetic_ground_truth(x=x, ground_truth=X.adjoint(ground_truth_field).val, x_max_pn=np.max(np.log(1+z_p)))
+plot_synthetic_ground_truth(x=x, ground_truth=X.adjoint(ground_truth_field).val, x_max_pn=np.max(np.log(1+z_p)), save=False)
 plot_synthetic_data(neg_scale_fac_mag=neg_a_mag, data=d.val, x_max_pn=np.max(np.log(1+z_p)),
-                    mu_arrays=np.concatenate((mu_p, mu_u)))
+                    mu_arrays=np.concatenate((mu_p, mu_u)), save=False)
 
 likelihood_energy = ift.GaussianEnergy(d, N.inverse) @ R
 global_iterations = 6
-"""
+
+
 posterior_samples = ift.optimize_kl(likelihood_energy=likelihood_energy,
                                     total_iterations=global_iterations,
                                     n_samples=kl_sampling_rate,
@@ -27,13 +28,13 @@ posterior_samples = ift.optimize_kl(likelihood_energy=likelihood_energy,
                                     return_final_position=False)
 
 # Save the inference run
-pickle_me_this(f"synthetic/{arguments}", posterior_samples)
-"""
+#pickle_me_this(f"synthetic/{arguments}", posterior_samples)
+
 # Unpickle the last inference run
-posterior_samples = unpickle_me_this("synthetic/cfm_{'offset_mean': 0, 'offset_std': None, 'fluctuations': (1.8, 1.8), 'loglogavgslope': (-4, 1e-16), 'asperity': None, 'flexibility': None}_lm_{'slope': (2, 5), 'intercept': (30, 5)}.pickle")
+# posterior_samples = unpickle_me_this("synthetic/cfm_{'offset_mean': 0, 'offset_std': None, 'fluctuations': (1.8, 1.8), 'loglogavgslope': (-4, 1e-16), 'asperity': None, 'flexibility': None}_lm_{'slope': (2, 5), 'intercept': (30, 5)}.pickle")
 
 posterior_realizations_list = posterior_samples
 s_mean, s_var = posterior_realizations_list.sample_stat(s)
 
 plot_synthetic_ground_truth(x=x, ground_truth=X.adjoint(ground_truth_field).val, x_max_pn=np.max(np.log(1+z_p)),
-                            reconstruction=(X.adjoint(s_mean).val, np.sqrt(X.adjoint(s_var).val)))
+                            reconstruction=(X.adjoint(s_mean).val, np.sqrt(X.adjoint(s_var).val)), save=False)