23e85759375060bb9531bb81b7edb60ed4fea8bc
Iason Saganas <iason-saganas@iasonsaga@gmail.com>
2026-03-05 17:08:12 +0100

    added pointwise CDF to dict

--- charm2/__init__.py
+++ charm2/__init__.py
@@ -1,6 +1,19 @@
 from .helpers import *
-
+from scipy.stats import norm
+import numpy as np
 import matplotlib.pyplot as plt
+
 from pathlib import Path
 style_path = Path(__file__).parent / "style_components" / "standardStyle.mplstyle"
 plt.style.use(style_path)
+
+def pointwise_CDF(x):
+    return norm.cdf(x)
+
+def pointwise_CDF_and_derv(x):
+    return norm.cdf(x), 1/np.sqrt(2*np.pi)*np.exp(-1/2*x**2)
+
+from nifty8.pointwise import ptw_dict
+ptw_dict["CDF"] = pointwise_CDF, pointwise_CDF_and_derv
+
+# __all__ = ['ptw_dict']
